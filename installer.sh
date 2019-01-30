5432909b826f8f2ee2f285670518360f08600dbd
Defte <defteatwhiteflag@gmx.fr>
2019-01-30 11:45:18 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,6 +3,15 @@
 # Python3 librairy
 ###################
 pip3 install shodan python-nmap beautifulsoup4
+git clone https://github.com/rthalley/dnspython.git
+cd dnspython
+python3 setup.py install
+cd ../
+git clone https://github.com/joepie91/python-whois.git
+cd python-whois
+python3 setup.py install
+cd
+
 
 #################
 # Required tools
