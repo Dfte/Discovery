54fdf661e6e246c8ee0e6557f3eb00fbcfc661b3
Defte <defteatwhiteflag@gmx.fr>
2019-03-15 17:39:27 +0100

    Update parser.py

--- modules/parser.py
+++ modules/parser.py
@@ -1,1 +1,11 @@
-
+import json
+file = open("", "r+")
+for line in file.readlines() :
+  string = line.replace("\n", "")
+  string='{"timestamp":"1550794171","name":"0.175.17.109.rev.sfr.net","type":"a","value":"109.17.175.0"}'
+  load = json.loads(string)
+  if load["name"] :
+    name = load["name"]
+    if load["type"] :
+      typ = load["type"]
+      print(name, typ)
